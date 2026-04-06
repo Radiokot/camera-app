0c9d2d1b9bf3cb64b0007f22b9e78c0eb498da32
Oleg Koretsky <oleg@radiokot.com.ua>
2026-04-06 12:55:14 +0300

    Add PoC framed image capture

--- app/src/main/java/ua/com/radiokot/camerapp/CaptureScreen.kt
+++ app/src/main/java/ua/com/radiokot/camerapp/CaptureScreen.kt
@@ -8,9 +8,11 @@ import androidx.camera.core.SurfaceRequest
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.lifecycle.awaitInstance
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -20,18 +22,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.boundsInParent
+import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import androidx.lifecycle.compose.LocalLifecycleOwner
 
 @Composable
 fun CaptureScreen(
     captureUseCase: ImageCapture?,
-    onCaptureClicked: () -> Unit,
+    onCaptureClicked: (Size, Rect) -> Unit,
     modifier: Modifier = Modifier,
 ) = Box(
+    contentAlignment = Alignment.Center,
     modifier = modifier
 ) {
     val context = LocalContext.current
@@ -42,11 +52,12 @@ fun CaptureScreen(
 
     LaunchedEffect(Unit) {
         val provider = ProcessCameraProvider.awaitInstance(context)
-        val previewUseCase = Preview.Builder().build().apply {
-            setSurfaceProvider {
-                surfaceRequest = it
+        val previewUseCase =
+            Preview.Builder().build().apply {
+                setSurfaceProvider {
+                    surfaceRequest = it
+                }
             }
-        }
         provider.bindToLifecycle(
             lifecycleOwner = lifecycleOwner,
             cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA,
@@ -61,9 +72,6 @@ fun CaptureScreen(
         CameraXViewfinder(
             surfaceRequest = surfaceRequest!!,
             modifier = Modifier
-                .clickable(
-                    onClick = onCaptureClicked,
-                )
                 .fillMaxSize()
         )
     } else {
@@ -82,6 +90,42 @@ fun CaptureScreen(
             )
         }
     }
+
+    var frameLayoutCoordinates by remember {
+        mutableStateOf<LayoutCoordinates?>(null)
+    }
+
+    Box(
+        modifier = Modifier
+            .size(
+                width = 26.dp * 5,
+                height = 37.dp * 5,
+            )
+            .clickable(
+                enabled =
+                    surfaceRequest != null && frameLayoutCoordinates != null,
+                onClick = {
+                    val viewfinderSize =
+                        frameLayoutCoordinates!!
+                            .parentLayoutCoordinates!!
+                            .size
+                            .toSize()
+                    val frameRect = frameLayoutCoordinates!!.boundsInParent()
+                    println(
+                        "OOLEG clicked ${viewfinderSize}, b ${frameRect}," +
+                                "preview res ${surfaceRequest!!.resolution}"
+                    )
+                    onCaptureClicked(viewfinderSize, frameRect)
+                },
+            )
+            .border(
+                width = 2.dp,
+                color = Color.Red,
+            )
+            .onPlaced { layoutCoordinates ->
+                frameLayoutCoordinates = layoutCoordinates
+            }
+    )
 }
 
 @Composable
@@ -101,7 +145,7 @@ private fun CaptureScreenPreview(
 ) {
     CaptureScreen(
         captureUseCase = null,
-        onCaptureClicked = {},
+        onCaptureClicked = { _, _ -> },
         modifier = Modifier
             .fillMaxSize()
     )
--- app/src/main/java/ua/com/radiokot/camerapp/CaptureScreenViewModel.kt
+++ app/src/main/java/ua/com/radiokot/camerapp/CaptureScreenViewModel.kt
@@ -1,14 +1,28 @@
 package ua.com.radiokot.camerapp
 
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.graphics.Paint
+import android.graphics.RectF
 import android.view.Surface
 import androidx.camera.core.ExperimentalZeroShutterLag
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.core.resolutionselector.ResolutionStrategy
 import androidx.camera.core.takePicture
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.core.graphics.applyCanvas
+import androidx.core.graphics.createBitmap
+import androidx.core.graphics.times
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlin.math.min
+
 
 @Immutable
 class CaptureScreenViewModel : ViewModel() {
@@ -18,21 +32,82 @@ class CaptureScreenViewModel : ViewModel() {
         ImageCapture.Builder()
             .setCaptureMode(ImageCapture.CAPTURE_MODE_ZERO_SHUTTER_LAG)
             .setTargetRotation(Surface.ROTATION_0)
+            .setResolutionSelector(
+                ResolutionSelector.Builder()
+                    .setResolutionStrategy(
+                        ResolutionStrategy(
+                            android.util.Size(1920, 1920),
+                            ResolutionStrategy.FALLBACK_RULE_CLOSEST_LOWER_THEN_HIGHER,
+                        )
+                    )
+                    .build()
+            )
             .build()
 
-    fun onCaptureClicked() {
-        takePicture()
+    fun onCaptureClicked(
+        visibleViewfinderSize: Size,
+        visibleFrameRect: Rect,
+    ) {
+        takePicture(
+            visibleViewfinderRect =
+                RectF(0f, 0f, visibleViewfinderSize.width, visibleViewfinderSize.height),
+            visibleFrameRect =
+                RectF(
+                    visibleFrameRect.left,
+                    visibleFrameRect.top,
+                    visibleFrameRect.right,
+                    visibleFrameRect.bottom
+                ),
+        )
     }
 
     private var takePictureJob: Job? = null
-    private fun takePicture() {
+    private fun takePicture(
+        visibleViewfinderRect: RectF,
+        visibleFrameRect: RectF,
+    ) {
         takePictureJob?.cancel()
-        takePictureJob = viewModelScope.launch {
-            captureUseCase.takePicture().use { imageProxy ->
-                val bitmap = imageProxy.toBitmap()
-                println("OOLEG captured, rotate ${imageProxy.imageInfo.rotationDegrees} degrees")
-                bitmap.recycle()
-            }
+        takePictureJob = viewModelScope.launch(Dispatchers.Default) {
+            val imageProxy = captureUseCase.takePicture()
+            val imageBitmap = imageProxy.toBitmap()
+            val rotatedImageBitmap = Bitmap.createBitmap(
+                imageBitmap,
+                0, 0,
+                imageProxy.width,
+                imageProxy.height,
+                Matrix().apply {
+                    setRotate(imageProxy.imageInfo.rotationDegrees.toFloat())
+                },
+                true
+            )
+
+            val frameScale = min(
+                rotatedImageBitmap.width / visibleViewfinderRect.width(),
+                rotatedImageBitmap.height / visibleViewfinderRect.height(),
+            )
+            val scaledViewfinderRect = visibleViewfinderRect * frameScale
+            val scaledFrameRect = visibleFrameRect * frameScale
+
+            val resultBitmap =
+                createBitmap(
+                    width = scaledFrameRect.width().toInt(),
+                    height = scaledFrameRect.height().toInt(),
+                ).applyCanvas {
+                    drawBitmap(
+                        rotatedImageBitmap,
+                        -(rotatedImageBitmap.width - scaledViewfinderRect.width()) / 2f
+                                - scaledFrameRect.left,
+                        -(rotatedImageBitmap.height - scaledViewfinderRect.height()) / 2f
+                                - scaledFrameRect.top,
+                        Paint(Paint.ANTI_ALIAS_FLAG),
+                    )
+                }
+
+            imageProxy.close()
+            imageBitmap.recycle()
+            rotatedImageBitmap.recycle()
+
+            println("OOLEG done $resultBitmap")
         }
     }
 }
