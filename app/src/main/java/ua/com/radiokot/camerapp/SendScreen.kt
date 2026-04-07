0fa1c5b1805255398f44545c14a5137328853b21
Oleg Koretsky <oleg@radiokot.com.ua>
2026-04-07 17:04:18 +0300

    Add the send button

--- app/src/main/java/ua/com/radiokot/camerapp/SendScreen.kt
+++ app/src/main/java/ua/com/radiokot/camerapp/SendScreen.kt
@@ -1,12 +1,21 @@
 package ua.com.radiokot.camerapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.draw.dropShadow
 import androidx.compose.ui.geometry.Offset
@@ -14,15 +23,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.shadow.Shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SendScreen(
     frameImage: ImageBitmap?,
     modifier: Modifier = Modifier,
 ) = Box(
-    contentAlignment = Alignment.Center,
     modifier = modifier
         .drawWithCache {
             val backgroundColor = Color(0xfffef6eb)
@@ -66,24 +78,73 @@ fun SendScreen(
             }
         }
 ) {
-    Box(
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .size(FrameSize * 1.5f)
-            .dropShadow(
-                shape = RectangleShape,
-                shadow = Shadow(
-                    radius = 16.dp,
-                    spread = 8.dp,
-                    color = Color(0x10000000),
-                )
-            )
+            .fillMaxSize()
+            .safeContentPadding()
     ) {
-        if (frameImage != null) {
-            Image(
-                bitmap = frameImage,
-                contentDescription = null,
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .weight(1f, true)
+        ) {
+            Box(
                 modifier = Modifier
-                    .fillMaxSize()
+                    .size(FrameSize * 1.5f)
+                    .dropShadow(
+                        shape = RectangleShape,
+                        shadow = Shadow(
+                            radius = 16.dp,
+                            spread = 8.dp,
+                            color = Color(0x10000000),
+                        )
+                    )
+            ) {
+                if (frameImage != null) {
+                    Image(
+                        bitmap = frameImage,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxSize()
+                    )
+                }
+            }
+        }
+
+        val buttonShape = RoundedCornerShape(
+            percent = 50,
+        )
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .padding(24.dp)
+                .height(56.dp)
+                .fillMaxWidth()
+                .dropShadow(
+                    shape = buttonShape,
+                    shadow = Shadow(
+                        radius = 8.dp,
+                        spread = 6.dp,
+                        color = Color(0x20000000),
+                    )
+                )
+                .clip(buttonShape)
+                .background(
+                    color = Color(0xffe5920f),
+                )
+        ) {
+            BasicText(
+                text = "Send",
+                style = TextStyle(
+                    textAlign = TextAlign.Center,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color(0xFF212121),
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
             )
         }
     }
