091f5083feee407edf5db47c3e4cd80d4b0b7f95
Oleg Koretsky <oleg@radiokot.com.ua>
2026-04-15 10:51:11 +0300

    Add le button component

---
+++ app/src/main/java/ua/com/radiokot/camerapp/LeButton.kt
@@ -0,0 +1,178 @@
+package ua.com.radiokot.camerapp
+
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LeButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable BoxScope.() -> Unit,
+) {
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+    val hapticFeedback by rememberUpdatedState(LocalHapticFeedback.current)
+    val isPressed by produceState(false) {
+        interactionSource.interactions.collect { interaction ->
+            if (interaction is PressInteraction.Press) {
+                hapticFeedback.performHapticFeedback(
+                    HapticFeedbackType.VirtualKey
+                )
+                value = true
+            } else if (interaction is PressInteraction.Release
+                || interaction is PressInteraction.Cancel
+            ) {
+                value = false
+            }
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .heightIn(
+                min = 64.dp,
+            )
+            .height(IntrinsicSize.Min)
+            .clickable(
+                indication = null,
+                interactionSource = interactionSource,
+                onClick = onClick,
+            )
+    ) {
+        val shape = remember {
+            RoundedCornerShape(10.dp)
+        }
+        val depth = 10.dp
+
+        Spacer(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = depth,
+                )
+                .background(
+                    color = Color(0xFFcbc4bb),
+                    shape = shape
+                )
+                .border(
+                    width = 2.dp,
+                    color = Color(0xFF6B624B),
+                    shape = shape,
+                )
+        )
+
+        val pressOffset by animateDpAsState(
+            targetValue =
+                if (isPressed)
+                    depth * 0.5f
+                else
+                    0.dp,
+            animationSpec = spring(
+                stiffness = Spring.StiffnessHigh,
+            ),
+        )
+
+        Box(
+            contentAlignment = Alignment.Center,
+            content = content,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    bottom = depth,
+                )
+                .graphicsLayer {
+                    translationY = pressOffset.toPx()
+                }
+                .background(
+                    color = Color(0xFFfff9eb),
+                    shape = shape,
+                )
+                .border(
+                    width = 2.dp,
+                    color = Color(0xFF6B624B),
+                    shape = shape,
+                )
+        )
+    }
+}
+
+@Composable
+fun LeTextButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+) {
+    LeButton(
+        modifier = modifier,
+        onClick = onClick,
+    ) {
+        BasicText(
+            text = text,
+            style = TextStyle(
+                textAlign = TextAlign.Center,
+            ),
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(10.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun LeButtonPreview(
+
+) {
+    var counter by remember {
+        mutableIntStateOf(0)
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .paperBackground()
+    ) {
+        LeTextButton(
+            text = "Me clicked $counter time(s)",
+            onClick = { counter++ },
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(24.dp)
+        )
+    }
+}
--- app/src/main/java/ua/com/radiokot/camerapp/SaveScreen.kt
+++ app/src/main/java/ua/com/radiokot/camerapp/SaveScreen.kt
@@ -3,20 +3,16 @@ package ua.com.radiokot.camerapp
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.IntState
 import androidx.compose.runtime.State
@@ -24,18 +20,13 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.dropShadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.shadow.Shadow
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
@@ -117,40 +108,13 @@ fun SaveScreen(
             percent = 50,
         )
 
-        Box(
-            contentAlignment = Alignment.Center,
+        LeTextButton(
+            text = "Save",
+            onClick = onSaveClicked,
             modifier = Modifier
                 .padding(24.dp)
-                .height(56.dp)
                 .fillMaxWidth()
-                .dropShadow(
-                    shape = buttonShape,
-                    shadow = Shadow(
-                        radius = 8.dp,
-                        spread = 2.dp,
-                        color = Color(0x414A6585),
-                    )
-                )
-                .clip(buttonShape)
-                .background(
-                    color = Color(0xffe5920f),
-                )
-                .clickable(
-                    onClick = onSaveClicked,
-                )
-        ) {
-            BasicText(
-                text = "Save to Gallery",
-                style = TextStyle(
-                    textAlign = TextAlign.Center,
-                    fontSize = 22.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    color = Color(0xFF212121),
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
-        }
+        )
     }
 }
 
